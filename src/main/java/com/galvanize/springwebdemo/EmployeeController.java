8abc7e94e18adc819f667556b7653d661889ad8b
Jim Cannamela <jim.cannamela@outlook.com>
2023-07-03 14:14:48 -0500

    Add Employee Test passing

---
+++ src/main/java/com/galvanize/springwebdemo/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.galvanize.springwebdemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+	List<Employee> employees = new ArrayList<>();
+
+	public EmployeeController() {
+		employees.add(new Employee(1, "First", "Employee"));
+		employees.add(new Employee(2, "Second", "Employee"));
+		employees.add(new Employee(3, "Third", "Employee"));
+		employees.add(new Employee(4, "Fourth", "Employee"));
+		employees.add(new Employee(5, "Fifth", "Employee"));
+	}
+
+	@GetMapping("/{id}")
+	public Employee findEmployeeById(@PathVariable int id) {
+		Employee employee = null;
+		for (Employee emp : employees) {
+			if (emp.getId() == id) {
+				employee = emp;
+			}
+		}
+
+		return employee;
+	}
+
+	@GetMapping("/search")
+	public List<Employee> findEmployeesByFirstAndLast(@RequestParam String firstName,
+													  @RequestParam String lastName){
+		List<Employee> searchResults = new ArrayList<>();
+		for(Employee emp : employees){
+			if(emp.getFirstName().equals(firstName)
+					&& emp.getLastName().equals(lastName)){
+				searchResults.add(emp);
+			}
+		}
+		return searchResults;
+	}
+
+	@PostMapping("/add")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addEmployee(@RequestBody Employee newEmployee) {
+		employees.add(newEmployee);
+	}
+}
--- src/test/java/com/galvanize/springwebdemo/EmployeeEndpointTests.java
+++ src/test/java/com/galvanize/springwebdemo/EmployeeEndpointTests.java
@@ -1,4 +1,54 @@
 package com.galvanize.springwebdemo;
 
-public class EmployeeEndpointTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class EmployeeEndpointTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void employeeWithId_returns_Employee() throws Exception {
+		mockMvc.perform(get("/api/employee/3"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(3))
+				.andExpect(jsonPath("$.firstName").value("Third"))
+				.andExpect(jsonPath("$.lastName").value("Employee"));
+	}
+
+	@Test
+	void employeeWithFirstNameAndLastNameParameters_returns_EmployeeWithinArray() throws Exception {
+		mockMvc.perform(get("/api/employee/search")
+						.param("firstName", "Fifth")
+						.param("lastName", "Employee"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].id").value(5))
+				.andExpect(jsonPath("$[0].firstName").value("Fifth"))
+				.andExpect(jsonPath("$[0].lastName").value("Employee"));
+	}
+
+	@Test
+	void employeeAdd_savesNewEmployee() throws Exception {
+		String json = "{ \"id\": \"6\", \"firstName\": \"Sixth\", \"lastName\": \"Employee\" }";
+		mockMvc.perform(post("/api/employee/add")
+						.contentType(MediaType.APPLICATION_JSON).content(json))
+				.andDo(print())
+				.andExpect(status().isCreated());
+		mockMvc.perform(get("/api/employee/6"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(6))
+				.andExpect(jsonPath("$.firstName").value("Sixth"))
+				.andExpect(jsonPath("$.lastName").value("Employee"));
+	}
 }
